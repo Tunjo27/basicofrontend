ce98dc9e6ccbb2f758e84ac61f2004afa2133bea
cristian Sosa <crissosa9927@gmail.com>
2025-02-02 11:06:08 -0500

    C:/Program Files/Git/home por defecto para cuando me loguee correctamente

--- src/components/Body/Login/Login.jsx
+++ src/components/Body/Login/Login.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import Modal from 'react-modal';
 import './Login.css';
+import { useNavigate } from 'react-router-dom';
 
 // Establece el elemento de la aplicación para el modal de React
 Modal.setAppElement('#root');
@@ -19,6 +20,7 @@ const Login = ({ isOpen, onRequestClose, onLogin }) => {
   // Estados para almacenar el nombre de usuario y la contraseña
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
   // Maneja el cambio en el campo de nombre de usuario
   const handleUsernameChange = (event) => {
@@ -33,9 +35,11 @@ const Login = ({ isOpen, onRequestClose, onLogin }) => {
   // Maneja el evento de inicio de sesión
   const handleLogin = () => {
     // Verificar si las credenciales son válidas
-    if (username === 'usuario' && password === 'contrasena') {
+    if (username === 'u' && password === 'p') {
       // Si las credenciales son válidas, llama a la función onRequestClose para cerrar el modal de inicio de sesión
       onLogin({ username }); // Aquí puedes pasar más información del usuario si lo deseas
+      onRequestClose();
+      navigate('/');
     } else {
       // Si las credenciales no son válidas, muestra un mensaje de error o toma la acción apropiada
       alert('Credenciales incorrectas. Inténtalo de nuevo.');
